f87e79812b1b0d0e109d4fdebbeeceb7479701f5
Idkwhattonamethis <74823075+idkwhattonamethislol@users.noreply.github.com>
2021-05-24 08:24:25 +0300

    intro to shell

--- src/init/initsh.py
+++ src/init/initsh.py
@@ -1,3 +1,6 @@
 import os
+from time import sleep
 def clearshell():
+    print("""Welcome to the python shell. Use this shell without root privileges to avoid risks. See the github repo at https://github.com/idkwhattonamethislol/python-shell""")
+    sleep(5.0)
     os.system("clear")
